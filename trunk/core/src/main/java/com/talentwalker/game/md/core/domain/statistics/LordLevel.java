72471d32db868a15fb89a1bcbc336abe73459336
zhangfutao <1875381433@qq.com>
2017-02-23 11:27:49 +0800

    玩家等级统计

---
+++ trunk/core/src/main/java/com/talentwalker/game/md/core/domain/statistics/LordLevel.java
@@ -0,0 +1,19 @@
+/**
+ * @Title: LordLevel.java
+ * @Copyright (C) 2017 太能沃可
+ * @Description:
+ * @Revision History:
+ * @Revision 1.0 2017年2月22日  张福涛
+ */
+
+package com.talentwalker.game.md.core.domain.statistics;
+
+/**
+ * @ClassName: LordLevel
+ * @Description: 玩家等级统计
+ * @author <a href="mailto:zhangfutao@talentwalker.com">张福涛</a> 于 2017年2月22日 下午6:14:36
+ */
+
+public class LordLevel {
+
+}
